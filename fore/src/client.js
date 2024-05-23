af79ac444b26d558d07815a6a286e9b55b5da604
apanakkat <asheem@foreai.co>
2024-05-23 10:00:20 +0200

    Add await for the generateFncall.

--- fore/src/client.js
+++ fore/src/client.js
@@ -253,7 +253,7 @@ class Foresight {
 			const outputs = {};
 
 			for (const [entryId, query] of Object.entries(queries)) {
-				const { generatedResponse, contexts } = generateFn(query);
+				const { generatedResponse, contexts } = await generateFn(query);
 
 				outputs[entryId] = {
 					generated_response: generatedResponse,
